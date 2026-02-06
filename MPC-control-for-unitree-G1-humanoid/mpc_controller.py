399b583bc28f3018e335c4e05b2e8e0ae2c5294a
Willmer Wohlén <willmer@wohlen.se>
2026-02-06 17:40:04 +0100

    mpc coding

--- MPC-control-for-unitree-G1-humanoid/mpc_controller.py
+++ MPC-control-for-unitree-G1-humanoid/mpc_controller.py
@@ -1,27 +1,60 @@
 import numpy as np
+from scipy.optimize import minimize
 import matplotlib.pyplot as plt
+
 
 class ModelPredictiveController:
 
     def __init__(self, pred_h, cont_h, Q, R, R_d, P, sampling_time):
         self.pred_h = pred_h
         self.cont_h = cont_h
-        self.Q = Q
-        self.R = R
-        self.R_d = R_d
-        self.P = P
+        self.Q = Q #state error cost
+        self.R = R #control cost
+        self.R_d = R_d #smooth control cost
+        self.P = P # terminal state python
         self.sampling_time = sampling_time
 
-    def cost(u, x, traj):
+    def cost(self,u, x, x_r):
 
         c = 0
+
         for i in range(self.pred_h):
-            x_k = model(u)
 
-            x_r = traj[i]
+            u_k = u[i*3:i+1*3]
 
-            state_e_cost = (x_k-x_r).T @ self.Q @ (x_k - x_r)
-            cont_cost = u.T @ self.R @ u
+            x_k = model(u_k,x)
+
+            print(x_k.T.shape)
+            print(x_r.shape)
+
+            state_e_cost = (x_k-x_r) @ self.Q @ (x_k - x_r).T
+            cont_cost = u_k.T @ self.R @ u_k
+
+        return c
+
+    def calc_constraints(self,map):
+        pass
+
+    def next_u(self,u,x, x_r, map, u_bounds):
+
+        nu = len(u) # number of inputs
+        u0 = np.zeros(self.pred_h * nu) #initial guess
+
+        #state_constraints = calc_constraints(map)
+
+        input_bounds = [u_bounds] * (self.pred_h * nu)
+
+        res = minimize(self.cost,
+                     u0,
+                     args=(x,x_r),
+                     method="SLSQP",
+                     bounds = input_bounds)
+
+        u_n = res.x[:nu]
+
+        return u_n
+
+
 
 
 
@@ -29,9 +62,7 @@ def model(u, x):
 
     x_n = x + u
 
-    y = x_n
-
-    return y, x_n
+    return x_n
 
 
 def main():
@@ -40,10 +71,29 @@ def main():
 
     x = np.zeros(3)
     u = 1
+
+    goal = np.array([1,1,1])
+
+    Q = 10
+    R = 10
+    pred_h = 10
+    cont_h = 10
+
+    mpc = ModelPredictiveController(pred_h,cont_h,Q,R,0,0,0)
+
+    u0 = np.zeros(3)
+
+    u_bounds = (-0.1,0.1)
     while True:
 
-        y, x = model(u,x)
-        print(y)
+        u = mpc.next_u(u0,x,goal,None,u_bounds)
+
+        print(u)
+
+
+        break
+
+
 
 main()
 
